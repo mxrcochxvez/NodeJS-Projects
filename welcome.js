2a562d9779461401797812e976beb56402a6539d
Marco Chavez <43889446+mxrcochxvez@users.noreply.github.com>
2020-06-04 12:48:40 -0700

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -1,15 +1,18 @@
 let args = process.argv;
+// removes "node" and the filename from the arguments.
 args.shift();
 args.shift();
 
+//Controls the direction of the application
 args[0] !== '-fn' ? guide() : application();
 
+//Provides usage details
 function guide() {
     console.log("Here are some helpful commands to pass when you run this file \n -fn 'First name'\n -ln 'Last name'");
     return;
 }
 
-
+//This is the application that takes the system arguments and sends you back a welcome message.
 function application() {
     var firstName, lastName;
 
